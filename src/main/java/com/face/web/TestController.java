7d476e77d505f6e7d0c28eb3b93214213219d292
xrl6238 <3jR0$am8>
2018-08-27 15:28:41 +0800

    init

---
+++ src/main/java/com/face/web/TestController.java
@@ -0,0 +1,19 @@
+package com.face.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("test")
+    public String test(ModelMap modelMap){
+        String libraryPath = System.getProperty("java.library.path");
+        String projectPath = System.getProperty("user.dir");
+        modelMap.put("libraryPath",libraryPath);
+        modelMap.put("projectPath",projectPath);
+
+        return "test";
+    }
+}
