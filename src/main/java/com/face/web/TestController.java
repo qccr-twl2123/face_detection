6cf73a4e57c602d49b841653b4b67df6a35584bb
xrl6238 <3jR0$am8>
2018-08-27 15:28:56 +0800

    init

--- src/main/java/com/face/web/TestController.java
+++ src/main/java/com/face/web/TestController.java
@@ -3,7 +3,6 @@ package com.face.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
 public class TestController {
 
